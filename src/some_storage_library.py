740c4fa4196883a896adf0bf8726d65b445659b0
Joshua Schlichting <7680545+JoshuaSchlichting@users.noreply.github.com>
2021-07-21 11:51:27 -0400

    addressed creation of the destination/ dir

--- src/some_storage_library.py
+++ src/some_storage_library.py
@@ -4,14 +4,17 @@ import shutil
 
 
 project_root = pathlib.Path(__file__).parent.parent.resolve() 
+destination = os.path.join(project_root, 'data', 'destination')
 
 
 class SomeStorageLibrary:
 
     def __init__(self) -> None:
-        print('Instantiating database library...')
+        print('Instantiating storage library...')
+        if not os.path.isdir(destination):
+            os.mkdir(destination)
 
     def load_csv(self, filename: str) -> None:
         print(f'Loading the following file to storage medium: {filename}')
-        shutil.move(filename, os.path.join(project_root, 'data', 'destination'))
+        shutil.move(filename, destination)
         print('Load completed!')
