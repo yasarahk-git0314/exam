bd68d6815b78dfc61f906005337e634452965baf
yasarahk-git0314 <87818203+yasarahk-git0314@users.noreply.github.com>
2021-07-22 21:47:34 +0530

    Update main.py with code

--- main.py
+++ main.py
@@ -8,3 +8,14 @@ expected csv file should exist in ../data/destination/ after the execution is co
 if __name__ == '__main__':
     """Entrypoint"""
     print('Beginning the ETL process...')
+    with open('data/SOURCE/SOURCECOLUMNS.txt') as f:
+        columns = [column.rstrip().split('|') for column in f]
+    with open('data/SOURCE/SOURCEDATA.txt') as f:
+        values = [value.rstrip().replace('|',',') for value in f]
+    columns = sorted(columns, key=lambda x: int(x[0]))
+    values.insert(0, ','.join([l[1] for l in columns]))
+    with open('data/DESTINATION/DESTINATIONDATA.csv', 'w+') as f:     
+    # write elements of list to destination file
+    for rows in values:
+        f.write('%s\n' %rows)
+    f.close()
